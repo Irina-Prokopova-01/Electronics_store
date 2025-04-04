54d7d015a3b6e069e53c1f308e666bc75d1c1253
Irina Prokopova <irina-prokopova.style@yandex.ru>
2025-04-04 10:55:45 +0300

    somme correct

--- supplier/views.py
+++ supplier/views.py
@@ -1,12 +1,12 @@
 from rest_framework import viewsets
 # from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from supplier.models import (Contacts, Factory, Individual_entrepreneur,
                              Products, Retail_chain)
 from supplier.paginators import SupplierPagination
-# from users.permissions import IsActiveUser
+from users.permissions import IsActiveUser
 from supplier.serializers import (ContactsSerializer, FactorySerializer,
                                   IndividualSerializer, ProductsSerializer,
                                   RetailSerializer)
@@ -18,7 +18,7 @@ class ProductsViewSet(viewsets.ModelViewSet):
     queryset = Products.objects.all()
     serializer_class = ProductsSerializer
     pagination_class = SupplierPagination
-    permission_classes = [AllowAny]
+    permission_classes = [IsActiveUser]
 
 
 class ContactsViewSet(viewsets.ModelViewSet):
@@ -27,7 +27,7 @@ class ContactsViewSet(viewsets.ModelViewSet):
     queryset = Contacts.objects.all()
     serializer_class = ContactsSerializer
     pagination_class = SupplierPagination
-    permission_classes = [AllowAny]
+    permission_classes = [IsActiveUser]
 
 
 class FactoryViewSet(viewsets.ModelViewSet):
@@ -36,7 +36,7 @@ class FactoryViewSet(viewsets.ModelViewSet):
     queryset = Factory.objects.all()
     serializer_class = FactorySerializer
     pagination_class = SupplierPagination
-    permission_classes = [AllowAny]
+    permission_classes = [IsActiveUser]
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ["contacts__country"]
 
@@ -47,7 +47,7 @@ class IndividualViewSet(viewsets.ModelViewSet):
     queryset = Individual_entrepreneur.objects.all()
     serializer_class = IndividualSerializer
     pagination_class = SupplierPagination
-    permission_classes = [AllowAny]
+    permission_classes = [IsActiveUser]
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ["contacts__country"]
 
@@ -58,6 +58,6 @@ class RetailViewSet(viewsets.ModelViewSet):
     queryset = Retail_chain.objects.all()
     serializer_class = RetailSerializer
     pagination_class = SupplierPagination
-    permission_classes = [AllowAny]
+    permission_classes = [IsActiveUser]
     filter_backends = [SearchFilter, OrderingFilter]
     search_fields = ["contacts__country"]
